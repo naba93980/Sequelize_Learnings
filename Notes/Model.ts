729e88691fc556132d5b6c8061a00c191b022c8e
NabajyotiModak <naba93981@gmail.com>
2023-03-02 23:29:43 +0530

    methods in Sequelize and Model class

---
+++ Notes/Model.ts
@@ -0,0 +1,65 @@
+/*
+public class Model {
+  public static addScope();
+  public static belongsTo();
+  public static belongsToMany();
+  public static build();
+  public static getAttributes();
+  public static getTableName();
+  public static hasMany();
+  public static hasOne();
+  public static init();
+  public static removeAttribute();
+  public static unscoped();
+  public static scope();
+  public static schema();
+  public static async aggregate();
+  public static async bulkCreate();
+  public static async count();
+  public static async create();
+  public static async decrement();
+  public static async describe();
+  public static async destroy();
+  public static async drop();
+  public static async findAll();
+  public static async findAndCountAll();
+  public static async findByPk();
+  public static async findCreateFind();
+  public static async findOne();
+  public static async findOrBuild();
+  public static async findOrCreate();
+  public static async increment();
+  public static async max();
+  public static async min();
+  public static async restore();
+  public static async sum();
+  public static async sync();
+  public static async truncate();
+  public static async update();
+  public static async upsert();
+
+  public Model();
+
+  public isNewRecord;
+  public get sequelize;
+
+  public changed();
+  public equals();
+  public equalsOneOf();
+  public get();
+  public getDataValue();
+  public isSoftDeleted();
+  public set();
+  public setDataValue();
+  public toJSON();
+  public where();
+  public async update();
+  public async validate();
+  public async save();
+  public async restore();
+  public async reload();
+  public async increment();
+  public async destroy();
+  public async decrement();
+}
+*/
---
+++ Notes/Sequelize.ts
@@ -0,0 +1,37 @@
+/*
+public class Sequelize {
+    public static and();
+    public static cast();
+    public static fn();
+    public static col();
+    public static json();
+    public static litera();
+    public static or();
+    public static where();
+
+    public Sequalize();
+
+    public models:{}
+
+    public close();
+    public define();
+    public escape();
+    public isDefined();
+    public model();
+    public random();
+    public async authenticate();
+    public async createSchema();
+    public async drop();
+    public async dropAllSchema();
+    public async dropSchema();
+    public async getDatabaseName();
+    public async getDialect();
+    public async getQueryInterface();
+    public async query();
+    public async set();
+    public async  truncate();
+    public async  showAllSchemas();
+    public async  sync();
+    public async  transaction();
+}
+*/
