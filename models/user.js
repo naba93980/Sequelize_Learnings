942a62cacf072420562adf7a06e07e401c226d1c
NabajyotiModak <naba93981@gmail.com>
2023-03-03 23:50:22 +0530

    User model (table)

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../connectDatabase');
+
+const User = sequelize.define("user", {
+    name: DataTypes.STRING,
+    email: {
+        type: DataTypes.STRING,
+        defaultValue: 'test@gmail.com',
+    },
+    gender: {
+        type: DataTypes.STRING,
+    },
+},
+    {
+        timestamps: true,
+        updatedAt: false,
+        createdAt: 'create-at',
+        tableName: 'users'
+    })
+
+module.exports = User
