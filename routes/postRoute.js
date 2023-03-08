3895a152d23386f23030013cc90ce3427fbdc46a
NabajyotiModak <naba93981@gmail.com>
2023-03-08 23:37:53 +0530

    many2many route

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router();
 
 const { getAllposts, getOnepost, createPost, updatepost, deletepost } = require('../controller/postController');
+const { Many2Many } = require('../controller/associationController');
 
 router.route('/posts').get(getAllposts);
 router.post('/post/new', createPost);
 router.route('/post/:id').get(getOnepost).put(updatepost).delete(deletepost);
+router.route('/post-tag/m2m').post(Many2Many);
 
 module.exports = router
