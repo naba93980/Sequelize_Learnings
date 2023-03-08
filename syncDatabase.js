5778b0788c1b4b1246d1a5e35aa3304c74bdd1fb
NabajyotiModak <naba93981@gmail.com>
2023-03-08 23:33:36 +0530

    many to many rekation in Tag and Post model

--- syncDatabase.js
+++ syncDatabase.js
@@ -1,4 +1,27 @@
 const { sequelize } = require('./connectDatabase');
+const Post = require('./models/post');
+const User = require('./models/user');
+const Tag = require('./models/tag');
+
+// Creating one -to-one relation
+
+// User.hasOne(Post,{
+//     foreignKey: 'userId'
+// });
+// Post.belongsTo(User);
+
+// Creating one -to-many relation
+
+// User.hasMany(Post,{
+//     foreignKey: 'userId'
+// });
+// Post.belongsTo(User);
+
+// Creating many-to-many relation
+
+Tag.belongsToMany(Post, { through: 'Post_Tag' });
+Post.belongsToMany(Tag, { through: 'Post_Tag' });
+
 
 const syncDatabase = async () => {
     try {
