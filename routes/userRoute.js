f4a54be22cc4dc6cd035bd2ff8c7c6bed234a5de
NabajyotiModak <naba93981@gmail.com>
2023-03-22 16:38:51 +0530

    one to one and many post routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,15 +3,16 @@ const router = express.Router();
 const { getAllUsers, getOneUser, createUser, updateUser, deleteUser } = require('../controller/userController');
 const query = require('../controller/queryController');
 const rawQuery = require('../controller/rawQueryController');
-const { One2One, BelongsToOne2One, One2Many } = require('../controller/associationController');
+const { One2OneGet, BelongsToOne2OneGet, One2ManyGet } = require('../controller/associationGetController');
+const { One2OnePost,One2ManyPost,BelongsToOne2OnePost } = require('../controller/associationPostController');
 
 router.route('/users').get(getAllUsers);
 router.post('/user/new', createUser);
 router.route('/user/:id').get(getOneUser).put(updateUser).delete(deleteUser);
 router.route('/query').get(query);
 router.route('/rawQuery').get(rawQuery);
-router.route('/o2o').get(One2One);
-router.route('/o2o-belongs2').get(BelongsToOne2One);
-router.route('/o2m').get(One2Many);
+router.route('/o2o').get(One2OneGet).post(One2OnePost);
+router.route('/o2o-belongs2').get(BelongsToOne2OneGet).post(BelongsToOne2OnePost);
+router.route('/o2m').get(One2ManyGet).post(One2ManyPost);
 
 module.exports = router
