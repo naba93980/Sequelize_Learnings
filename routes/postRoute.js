406c072d7534ab24ccbf5fc70626dcd1ddb15851
NabajyotiModak <naba93981@gmail.com>
2023-03-22 16:33:55 +0530

    many 2 many get post routes

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router();
 
 const { getAllposts, getOnepost, createPost, updatepost, deletepost } = require('../controller/postController');
-const { Many2Many } = require('../controller/associationController');
+const { Many2ManyGet } = require('../controller/associationGetController');
+const { Many2ManyPost } = require('../controller/associationPostController');
 
 router.route('/posts').get(getAllposts);
 router.post('/post/new', createPost);
 router.route('/post/:id').get(getOnepost).put(updatepost).delete(deletepost);
-router.route('/post-tag/m2m').post(Many2Many);
+router.route('/post-tag/m2m').get(Many2ManyGet).post(Many2ManyPost);
 
 module.exports = router
