be903a1977dd22597d224c0a696983221abe0449
NabajyotiModak <naba93981@gmail.com>
2023-03-26 22:35:53 +0530

    updated

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const { connectDB } = require('./connectDatabase');
 const { syncDatabase } = require('./syncDatabase.js');
 const userRouter = require('./routes/userRoute');
 const postRouter = require('./routes/postRoute');
+const imageRouter = require('./routes/imageRoute');
+const eagerlazyRoute = require('./routes/eagerlazyRoute')
+const transactionRouter = require('./routes/transaction')
+const queryInterfaceRouter = require('./routes/queryInterfaceRoute')
 
 app.get('/', (req, res) => {
     res.send("Home page");
@@ -13,6 +17,10 @@ app.get('/', (req, res) => {
 app.use(express.json())
 app.use('/api/v1',userRouter)
 app.use('/api/v1',postRouter)
+app.use('/api/v1',imageRouter)
+app.use('/api/v1',eagerlazyRoute)
+app.use('/api/v1',transactionRouter)
+app.use('/api/v1',queryInterfaceRouter)
 
 const startServer = async () => {
     const server = app.listen(PORT, () => {
