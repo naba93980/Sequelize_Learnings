0855791f77ebcf089c5e180f54a882fa0f920b01
NabajyotiModak <naba93981@gmail.com>
2023-03-27 19:13:31 +0530

    migration files

---
+++ migrations&seeders/migrations/20230326221039-create-student.js
@@ -0,0 +1,21 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Students');
+  }
+};
