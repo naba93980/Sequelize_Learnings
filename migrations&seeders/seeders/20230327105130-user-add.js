39fc231704c866844c4838708e1b24d1776a9529
NabajyotiModak <naba93981@gmail.com>
2023-03-27 19:14:06 +0530

    seeder files

---
+++ migrations&seeders/seeders/20230327105130-user-add.js
@@ -0,0 +1,27 @@
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('users', [{
+        email: 'naba93980@gmail.com',
+     }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
